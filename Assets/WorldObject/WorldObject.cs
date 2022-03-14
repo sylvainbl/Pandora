5976462b62331fa40f89bbc7cd8e810c88b430be
Sylvain Blanchard-Gaillard <sylvain.abbg@gmail.com>
2022-03-14 23:06:11 +0100

    added worldObject framework [4]

---
+++ Assets/WorldObject/WorldObject.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldObject : MonoBehaviour
+{
+    public string objectName;
+    public Texture2D buildImage;
+    public int cost, sellValue, hitPoints, maxHitPoints;
+
+
+    protected Player player;
+    protected string[] actions = {};
+    protected bool currentlySelected = false;
+
+    protected virtual void Awake() {
+    
+    }
+    
+    protected virtual void Start () {
+        player = transform.root.GetComponentInChildren< Player >();
+    }
+    
+    protected virtual void Update () {
+    
+    }
+    
+    protected virtual void OnGUI() {
+    }
+
+    public void SetSelection(bool selected) {
+        currentlySelected = selected;
+    }
+
+    
+    public string[] GetActions() {
+        return actions;
+    }
+    
+    public virtual void PerformAction(string actionToPerform) {
+        //it is up to children with specific actions to determine what to do with each of those actions
+    }
+}
