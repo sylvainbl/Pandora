505a9e5dfe6d53f4f896f6741184fd216d36ba5c
Sylvain Blanchard-Gaillard <sylvain.abbg@gmail.com>
2022-03-15 21:30:49 +0100

    added selection of buildings [5]

--- Assets/Player/HUD/HUD.cs
+++ Assets/Player/HUD/HUD.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 
 public class HUD : MonoBehaviour
 {
+    private Player player;
+    private const int SELECTION_NAME_HEIGHT = 15;
     public GUISkin resourceSkin, ordersSkin;
     private const int ORDERS_BAR_WIDTH = 150, RESOURCE_BAR_HEIGHT = 40;
-    private Player player;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +30,13 @@ public class HUD : MonoBehaviour
         GUI.skin = ordersSkin;
         GUI.BeginGroup(new Rect(Screen.width-ORDERS_BAR_WIDTH,RESOURCE_BAR_HEIGHT,ORDERS_BAR_WIDTH,Screen.height-RESOURCE_BAR_HEIGHT));
         GUI.Box(new Rect(0,0,ORDERS_BAR_WIDTH,Screen.height-RESOURCE_BAR_HEIGHT),"");
+        string selectionName = "";
+        if(player.SelectedObject) {
+            selectionName = player.SelectedObject.objectName;
+        }
+        if(!selectionName.Equals("")) {
+            GUI.Label(new Rect(0,10,ORDERS_BAR_WIDTH,SELECTION_NAME_HEIGHT), selectionName);
+        }
         GUI.EndGroup();
     }
 
@@ -38,5 +46,14 @@ public class HUD : MonoBehaviour
         GUI.Box(new Rect(0,0,Screen.width,RESOURCE_BAR_HEIGHT),"");
         GUI.EndGroup();
     }
+
+    public bool MouseInBounds() {
+        //Screen coordinates start in the lower-left corner of the screen
+        //not the top-left of the screen like the drawing coordinates do
+        Vector3 mousePos = Input.mousePosition;
+        bool insideWidth = mousePos.x >= 0 && mousePos.x <= Screen.width - ORDERS_BAR_WIDTH;
+        bool insideHeight = mousePos.y >= 0 && mousePos.y <= Screen.height - RESOURCE_BAR_HEIGHT;
+        return insideWidth && insideHeight;
+}
 
 }
--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -6,11 +6,13 @@ public class Player : MonoBehaviour
 {
     public string username;
     public bool human;
+    public HUD hud;
+    public WorldObject SelectedObject { get; set; }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        hud = GetComponentInChildren<HUD>();
     }
 
     // Update is called once per frame
